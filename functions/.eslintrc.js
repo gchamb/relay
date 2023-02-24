eb0bf46259e3f28ef7b66fc855fc9eb14b2afb7c
Giantte <gchambe2@emich.edu>
2023-02-23 22:51:26 -0500

    I ulted

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -22,7 +22,8 @@ module.exports = {
   ],
   plugins: ["@typescript-eslint", "import"],
   rules: {
-    "quotes": ["error", "double"],
+    quotes: ["error", "double"],
     "import/no-unresolved": 0,
+    "quote-props": ["warn", "as-needed"],
   },
 };
