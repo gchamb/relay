48eb15189dab664d2b03256f521d95f7cd0f7d73
Giantte <gchambe2@emich.edu>
2023-02-23 22:38:15 -0500

    fix workflow problems

--- web/src/firebase/app.ts
+++ web/src/firebase/app.ts
@@ -10,43 +10,40 @@ import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 let firebaseConfig: FirebaseOptions;
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 if (process.env.NEXT_PUBLIC_FIREBASE_CONFIG === "development") {
-    console.log("IN DEVV")
-    firebaseConfig = {
-        apiKey: "AIzaSyDUO5xq_DVO0mX6hHAQa2BnivDj0CztTQs",
-        authDomain: "relay-dc44e.firebaseapp.com",
-        projectId: "relay-dc44e",
-        storageBucket: "relay-dc44e.appspot.com",
-        messagingSenderId: "330991658176",
-        appId: "1:330991658176:web:5401e95790ee7a9da5b925",
-        measurementId: "G-B5CRDYV6G4"
-    };
+  console.log("IN DEVV");
+  firebaseConfig = {
+    apiKey: "AIzaSyDUO5xq_DVO0mX6hHAQa2BnivDj0CztTQs",
+    authDomain: "relay-dc44e.firebaseapp.com",
+    projectId: "relay-dc44e",
+    storageBucket: "relay-dc44e.appspot.com",
+    messagingSenderId: "330991658176",
+    appId: "1:330991658176:web:5401e95790ee7a9da5b925",
+    measurementId: "G-B5CRDYV6G4",
+  };
 } else if (process.env.NEXT_PUBLIC_FIREBASE_CONFIG === "production") {
-    console.log("IN PROD")
-    firebaseConfig = {
-        apiKey: "AIzaSyAJI0zq4FxnYfiHePccRZcgZGy5caOLEu4",
-        authDomain: "relay-bfaa1.firebaseapp.com",
-        projectId: "relay-bfaa1",
-        storageBucket: "relay-bfaa1.appspot.com",
-        messagingSenderId: "373269149161",
-        appId: "1:373269149161:web:bbc8c2db338bbe75910778",
-        measurementId: "G-YN1MR356KQ"
-    };
+  console.log("IN PROD");
+  firebaseConfig = {
+    apiKey: "AIzaSyAJI0zq4FxnYfiHePccRZcgZGy5caOLEu4",
+    authDomain: "relay-bfaa1.firebaseapp.com",
+    projectId: "relay-bfaa1",
+    storageBucket: "relay-bfaa1.appspot.com",
+    messagingSenderId: "373269149161",
+    appId: "1:373269149161:web:bbc8c2db338bbe75910778",
+    measurementId: "G-YN1MR356KQ",
+  };
 } else {
-    throw new Error("Couldn't load Firebase Config")
+  throw new Error("Couldn't load Firebase Config");
 }
-
-
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const firestore = getFirestore(app)
+export const firestore = getFirestore(app);
 // export const analytics = getAnalytics(app);
-export const test = "Testing"
+export const test = "Testing";
 
 if (process.env.NEXT_PUBLIC_USE_EMULATORS === "true") {
-    connectFirestoreEmulator(firestore, "localhost", 8080);
-    connectAuthEmulator(auth, "http://localhost:9099", { disableWarnings: true });
-    // connectFunctionsEmulator(functions, "localhost", 5001);
+  connectFirestoreEmulator(firestore, "localhost", 8080);
+  connectAuthEmulator(auth, "http://localhost:9099", { disableWarnings: true });
+  // connectFunctionsEmulator(functions, "localhost", 5001);
 }
