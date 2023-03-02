d88673c58f205c1c835bd7c6c53294b23292359f
Giantte <gchambe2@emich.edu>
2023-03-01 19:12:24 -0500

    refactor: :truck: rename function and reordering imports

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import type { AppProps } from "next/app";
 
 const inter = Inter({ subsets: ["latin"] });
 
