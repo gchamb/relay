f4531f4c2757c637b807836d687a98f245c6410b
Giantte <gchambe2@emich.edu>
2023-03-01 19:15:40 -0500

    feat: :sparkles: add functions related to the users collection

---
+++ web/src/lib/firebase/users.ts
@@ -0,0 +1,25 @@
+import { deleteDoc, doc, getDoc, setDoc } from "firebase/firestore";
+import { userCollection } from "./references/firestore";
+
+export const getUserDoc = async (userId: string) => {
+    const userDocRef = doc(userCollection, userId);
+    const userDoc = await getDoc(userDocRef);
+
+    return { userDoc, userDocRef };
+}
+
+export const createUserDoc = async (userId: string, nickname: string) => {
+    const userDocRef = doc(userCollection, userId);
+    await setDoc(userDocRef, {
+        nickname,
+        correct: 0,
+        incorrect: 0,
+        firstPlaces: 0
+    });
+}
+
+export const deleteUserDoc = async (userId: string) => {
+    const userDocRef = doc(userCollection, userId);
+    await deleteDoc(userDocRef)
+}
+
