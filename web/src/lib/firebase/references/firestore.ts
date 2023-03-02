e8c225b7b6d75d3fbdec5c3840be22c3b5427292
Giantte <gchambe2@emich.edu>
2023-03-02 17:30:15 -0500

    refactor: :recycle: remove unneeded import and rename users

--- web/src/lib/firebase/references/firestore.ts
+++ web/src/lib/firebase/references/firestore.ts
@@ -1,5 +1,5 @@
-import { CollectionReference, collection, doc, getDoc } from "firebase/firestore";
+import { CollectionReference, collection } from "firebase/firestore";
 import { firestore } from "../app";
 import { User } from "../firestore-types/users";
 
-export const userCollection = collection(firestore, "/users") as CollectionReference<User>;
+export const userCollection = collection(firestore, "users") as CollectionReference<User>;
