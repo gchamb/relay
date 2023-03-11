155e70d2352be443d0ecdadb3485d63e016bca2d
Giantte <gchambe2@emich.edu>
2023-03-11 17:14:15 -0500

    feat: :sparkles: add new function references

--- web/src/lib/firebase/references/functions.ts
+++ web/src/lib/firebase/references/functions.ts
@@ -1,7 +1,8 @@
 import { functions } from "../app";
 import { httpsCallable } from "firebase/functions";
-import { CreateGameRequest, CreateGameResponse, InvitePlayerRequest, JoinGameRequest } from "../function-types/types";
+import { CreateGameRequest, CreateGameResponse, DeleteInviteRequest, InvitePlayerRequest, JoinGameRequest } from "../function-types/types";
 
 export const createGame = httpsCallable<CreateGameRequest, CreateGameResponse>(functions, "createGame");
 export const joinGame = httpsCallable<JoinGameRequest, void>(functions, "joinGame");
 export const invitePlayer = httpsCallable<InvitePlayerRequest, void>(functions, "invitePlayer");
+export const deleteInvite = httpsCallable<DeleteInviteRequest, void>(functions, "deleteInvite");
