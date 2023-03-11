f5296dcd10241a6d8fc4facbb95bb374ec44ac12
Giantte <gchambe2@emich.edu>
2023-03-11 17:07:50 -0500

    feat: :sparkles: realtime invite updates and can join and delete them

--- functions/src/function-types/types.ts
+++ functions/src/function-types/types.ts
@@ -1,4 +1,5 @@
 import { Game, Topics, isCapacity, isTopic, uid } from "../firestore-types/game";
+import { User } from "../firestore-types/users";
 
 export type CreateGameRequest = { topic: Topics; capacity: Game["capacity"]; inviteOnly: boolean };
 export type CreateGameResponse = { gameId: string };
@@ -6,6 +7,8 @@ export type CreateGameResponse = { gameId: string };
 export type JoinGameRequest = { gameId: string };
 
 export type InvitePlayerRequest = { gameId: string; invitee: uid };
+
+export type DeleteInviteRequest = User["invites"][number];
 
 export function isCreateGameRequest(data: unknown): data is CreateGameRequest {
   if (data === null || data == undefined || typeof data !== "object") {
@@ -38,4 +41,17 @@ export function isInvitePlayerRequest(data: unknown): data is InvitePlayerReques
   }
 
   return "gameId" in data && typeof data.gameId === "string" && "invitee" in data && typeof data.invitee === "string";
+}
+
+export function isDeleteInviteRequest(data: unknown): data is DeleteInviteRequest {
+  if (data === null || data == undefined || typeof data !== "object") {
+    return false;
+  }
+
+
+  return "gameId" in data && typeof data.gameId === "string" &&
+    "host" in data && typeof data.host === "string" &&
+    "sentAt" in data && typeof data.sentAt === "object" &&
+    data.sentAt !== null && "nanoseconds" in data.sentAt &&
+    "seconds" in data.sentAt;
 }
--- web/src/components/InviteInbox.tsx
+++ web/src/components/InviteInbox.tsx
@@ -1,1 +1,149 @@
-export default function InviteInbox() {}
+import MyPopover from "./MyPopover";
+import MyToolTip from "./MyTooltip";
+import Loader from "./Loader";
+
+import { Check, Inbox, Trash2 } from "lucide-react";
+import { useSubscribeInvites } from "@/hooks/user";
+import { Separator } from "./ui/Separator";
+import { useMemo, useState } from "react";
+import { deleteInvite, joinGame } from "@/lib/firebase/references/functions";
+import { useRouter } from "next/router";
+import { FirebaseError } from "firebase/app";
+import { User } from "@/lib/firebase/firestore-types/users";
+
+export default function InviteInbox() {
+  const [readInvites, setReadInvites] = useState(0);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const invites = useSubscribeInvites();
+  const router = useRouter();
+  const unRead = useMemo(() => {
+    if (invites.length - readInvites < 0) {
+      setReadInvites(invites.length);
+      return 0;
+    }
+
+    return invites.length - readInvites;
+  }, [invites, readInvites]);
+
+  const joinGameHandler = async (gameId: string) => {
+    setLoading(true);
+
+    try {
+      await joinGame({ gameId });
+      router.push(`/games/${gameId}`);
+    } catch (err) {
+      if (err instanceof FirebaseError) {
+        setError(err.message);
+      } else {
+        setError(String(err));
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteInviteHandler = async (invite: User["invites"][number]) => {
+    setLoading(true);
+
+    try {
+      await deleteInvite(invite);
+    } catch (err) {
+      if (err instanceof FirebaseError) {
+        setError(err.message);
+      } else {
+        setError(String(err));
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <MyPopover
+      align="end"
+      slotTrigger={
+        <button
+          className="relative p-1"
+          onClick={() => {
+            if (readInvites === invites.length) {
+              return;
+            }
+
+            setReadInvites(invites.length);
+          }}
+        >
+          <Inbox className="cursor-pointer" />
+          <span className="sr-only">Invites</span>
+          {unRead > 0 && (
+            <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
+              {unRead}
+            </div>
+          )}
+        </button>
+      }
+      contentClassName="w-60"
+      beforeClosing={() => {
+        setError("");
+      }}
+      slotContent={
+        <>
+          {loading ? (
+            <Loader size={10} />
+          ) : (
+            <>
+              {invites.length !== 0 ? (
+                <div className="flex flex-col gap-y-2">
+                  <h2 className="font-semibold text-center text-md pb-1">Invites</h2>
+                  <Separator />
+                  <div className="flex flex-col gap-2">
+                    {error !== "" && <span className="text-sm text-center text-red-700 font-semibold">{error}</span>}
+                    {invites.map((invite, idx) => {
+                      return (
+                        <div className="flex justify-between items-center" key={idx}>
+                          <div className="flex flex-col ">
+                            <span>{invite.host}</span>
+                            <span className="opacity-40 text-sm">{invite.sentAt.toDate().toDateString()}</span>
+                            <span className="opacity-40 text-sm">{invite.sentAt.toDate().toLocaleTimeString()}</span>
+                          </div>
+
+                          <MyToolTip
+                            slotTrigger={
+                              <>
+                                <Check
+                                  className="w-5 cursor-pointer hover:text-green-500 focus:text-green-500"
+                                  onClick={async () => await joinGameHandler(invite.gameId)}
+                                />
+                                <span className="sr-only">Accept</span>
+                              </>
+                            }
+                            slotContent={<span>Accept</span>}
+                          />
+                          <MyToolTip
+                            slotTrigger={
+                              <>
+                                <Trash2
+                                  className="w-5 cursor-pointer hover:text-red-500 focus:text-red-500"
+                                  onClick={async () => await deleteInviteHandler(invite)}
+                                />
+                                <span className="sr-only">Delete</span>
+                              </>
+                            }
+                            slotContent={<span>Delete</span>}
+                          />
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              ) : (
+                <h2 className="font-semibold text-center text-md">No Invites!</h2>
+              )}
+            </>
+          )}
+        </>
+      }
+    />
+  );
+}
